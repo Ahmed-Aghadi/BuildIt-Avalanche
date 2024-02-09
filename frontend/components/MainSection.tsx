46572c6bbdb96ac57d5c67964fb09d8170ce80e9
ahmedaghadi <ahmedaghadi123@gmail.com>
2024-02-10 03:09:55 +0530

    chore: add wallet connected state

--- frontend/components/MainSection.tsx
+++ frontend/components/MainSection.tsx
@@ -1,6 +1,18 @@
 import { AuthConnectButton } from "@/components/AuthConnectButton";
+import { useAccount } from "@particle-network/connectkit";
+import { useEffect, useState } from "react";
 
 export const MainSection = () => {
+  const account = useAccount();
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    if (account) {
+      setConnected(true);
+    } else {
+      setConnected(false);
+    }
+  }, [account]);
   return (
     <div>
       <div className="absolute top-4 right-4">
