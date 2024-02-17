82f24aa16652aeb3ce4fa7ff191df9c869ba9141
ahmedaghadi <ahmedaghadi123@gmail.com>
2024-02-17 20:28:33 +0530

    fix: ssx component

--- frontend/components/SSXComponent.tsx
+++ frontend/components/SSXComponent.tsx
@@ -21,72 +21,12 @@ const SSXComponent = ({
   removeEventListener: any;
   sendMessage: any;
 }) => {
-  const [ssxProvider, setSSX] = useState<SSX | null>(null);
-
-  // const [formattedData, setFormattedData] = useState<Designs>([]);
-
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-
-  // useEffect(() => {
-  //   if (ssxProvider && !wasSet) {
-  //     setWasSet(true);
-  //   }
-  //   if (wasSet && !ssxProvider) {
-  //     ssxHandler();
-  //   }
-  // }, [wasSet, ssxProvider]);
-
-  const getData = async () => {
-    if (!ssxProvider) {
-      console.log("no ssxProvider getData");
-      return;
-    }
-    let { data } = await ssxProvider.storage.list();
-    data = data.filter((d: string) => d.includes("/content/"));
-    let datas = new Map();
-    for (let i = 0; i < data.length; i++) {
-      const contentName = data[i].replace("my-app/", "");
-      const { data: data1 } = await ssxProvider.storage.get(contentName);
-      const label = contentName.replace("content/", "");
-      datas.set(label, data1);
-      console.log(`${data1}`);
-    }
-    console.log("datas: ", datas);
-    let formattedData: Designs = [];
-    const keys = Array.from(datas.keys());
-    for (let i = 0; i < keys.length; i++) {
-      formattedData.push({
-        label: keys[i],
-        design: JSON.parse(datas.get(keys[i])),
-      });
-    }
-    console.log("formattedData: ", formattedData);
-    const formattedDataString = JSON.stringify(formattedData);
-    console.log("formattedDataString: ", formattedDataString);
-    return formattedData;
-  };
-
-  const saveDesign = useCallback(
-    async (design: string) => {
-      if (!ssxProvider) {
-        console.log("no ssxProvider saveDesign");
-        return;
-      }
-      const data: Design = JSON.parse(design);
-      const label = data.label;
-      const designString = JSON.stringify(data.design);
-      const contentName = `content/${label}`;
-      await ssxProvider.storage.put(contentName, designString);
-    },
-    [ssxProvider, unityProvider, isLoaded]
-  );
+  const [ssxProvider, setSsxProvider] = useState<SSX | null>(null);
 
   const ssxHandler = useCallback(async () => {
     if (ssxProvider) {
       console.log("ssxProvider already available ssxHandler");
-      return;
+      ssxProvider;
     }
     console.log("signing in...");
     const ssx = new SSX({
@@ -108,46 +48,87 @@ const SSXComponent = ({
       },
     });
     await ssx.signIn();
-    setSSX(ssx);
-  }, [ssxProvider, unityProvider, isLoaded]);
+    setSsxProvider(ssx);
+    return ssx;
+  }, [ssxProvider, provider]);
+
+  const getData = useCallback(async () => {
+    let ssx = ssxProvider;
+    if (!ssx) {
+      console.log("no ssxProvider getData");
+      ssx = await ssxHandler();
+    }
+    let { data } = await ssx.storage.list();
+    data = data.filter((d: string) => d.includes("/content/"));
+    let datas = new Map();
+    for (let i = 0; i < data.length; i++) {
+      const contentName = data[i].replace("my-app/", "");
+      const { data: data1 } = await ssx.storage.get(contentName);
+      const label = contentName.replace("content/", "");
+      datas.set(label, data1);
+      console.log(`${data1}`);
+    }
+    console.log("datas: ", datas);
+    let formattedData: Designs = [];
+    const keys = Array.from(datas.keys());
+    for (let i = 0; i < keys.length; i++) {
+      formattedData.push({
+        label: keys[i],
+        design: JSON.parse(datas.get(keys[i])),
+      });
+    }
+    console.log("formattedData: ", formattedData);
+    const formattedDataString = JSON.stringify(formattedData);
+    console.log("formattedDataString: ", formattedDataString);
+    return formattedData;
+  }, [ssxProvider, ssxHandler]);
+
+  const getDesigns = useCallback(async () => {
+    const data = await getData();
+    console.log("getDesigns", JSON.stringify(data));
+    sendMessage("CanvasManager", "SetDesigns", JSON.stringify(data));
+  }, [getData, sendMessage]);
+
+  const saveDesign = useCallback(
+    async (design: string) => {
+      let ssx = ssxProvider;
+      if (!ssx) {
+        console.log("no ssxProvider saveDesign");
+        ssx = await ssxHandler();
+      }
+      const data: Design = JSON.parse(design);
+      const label = data.label;
+      const designString = JSON.stringify(data.design);
+      const contentName = `content/${label}`;
+      await ssx.storage.put(contentName, designString);
+    },
+    [ssxProvider, ssxHandler]
+  );
 
   const ssxLogoutHandler = async () => {
     ssxProvider?.signOut();
-    setSSX(null);
+    setSsxProvider(null);
   };
 
   useEffect(() => {
-    // requestFullscreen(true);
-    //@ts-ignore
-    addEventListener("SignIn", ssxHandler);
-    //@ts-ignore
-    addEventListener("GetDesigns", getDesigns);
-    //@ts-ignore
-    addEventListener("SaveDesign", saveDesign);
-    getData();
+    if (provider) {
+      // addEventListener("SignIn", ssxHandler);
+      addEventListener("GetDesigns", getDesigns);
+      addEventListener("SaveDesign", saveDesign);
+    }
     return () => {
-      //@ts-ignore
-      removeEventListener("SignIn", ssxHandler);
-      //@ts-ignore
+      // removeEventListener("SignIn", ssxHandler);
       removeEventListener("GetDesigns", getDesigns);
-      //@ts-ignore
       removeEventListener("SaveDesign", saveDesign);
     };
   }, [
     addEventListener,
     removeEventListener,
+    provider,
     ssxHandler,
     getDesigns,
-    getData,
     saveDesign,
-    unityProvider,
-    isLoaded,
   ]);
-
-  async function getDesigns() {
-    console.log("getDesigns", JSON.stringify(await getData()));
-    sendMessage("CanvasManager", "SetDesigns", JSON.stringify(await getData()));
-  }
 
   return <></>;
 };
